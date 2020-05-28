85493ea7be621802998b31cadf39407fc5d1b85e
Warrick Mitchell <wyvernflyin@gmail.com>
2020-05-28 13:18:15 +0800

    Changed ping count requirement to speed things up

--- tests/RouterNapalm.py
+++ tests/RouterNapalm.py
@@ -19,7 +19,7 @@ def napalm_connect_cisco_ios(device, login, password, secret):
 
 @keyword('Ping Neighbor')
 def ping(device, peer_ip):
-    ping_results = device.ping(peer_ip, count=5)
+    ping_results = device.ping(peer_ip, count=2)
     for result in ping_results:
         if "success" in result:
             return "Successful"
